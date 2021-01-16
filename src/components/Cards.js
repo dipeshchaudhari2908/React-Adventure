13aadca41ef6a1494e9c75d2995fb363f7b8e6ee
Dipesh Chaudhari <chaudhari.dipesh3360@gmail.com>
2021-01-16 23:13:33 +0530

    images done

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css'
+import img9 from '../assets/images/img-9.jpg';
+import img2 from '../assets/images/img-2.jpg';
+import img4 from '../assets/images/img-4.jpg';
+import img5 from '../assets/images/img-5.jpg';
+import img6 from '../assets/images/img-6.jpg';
 
 function Cards() {
     return (
@@ -10,33 +15,33 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem 
-                        src="images/img-9.jpg"
+                        src={img9}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Adventure"
                         path="/services" 
                         />
                         <CardItem 
-                        src="images/img-2.jpg"
-                        text="Travel through the Island of Bali in a Private Cruise"
+                        src={img2}
+                        text="Travel through the Island of Bali in a Private Cruise and more"
                         label="Luxury"
                         path="/services" 
                         />
                     </ul>
                     <ul className="cards__items">
                         <CardItem 
-                        src="images/img-4.jpg"
+                        src={img4}
                         text="Explore the hidden waterfall deep inside the Amazon Jungle"
                         label="Adventure"
                         path="/services" 
                         />
                         <CardItem 
-                        src="images/img-5.jpg"
+                        src={img5}
                         text="Travel through the Island of Bali in a Private Cruise"
                         label="Luxury"
                         path="/services" 
                         />
                         <CardItem 
-                        src="images/img-6.jpg"
+                        src={img6}
                         text="Travel through the Island of Bali in a Private Cruise"
                         label="Luxury"
                         path="/services" 
